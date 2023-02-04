394486013b91543957b9a847586ad2ffc7be9fbb
leeqi10 <447789814@qq.com>
2023-02-04 23:33:22 +0800

    用户注册和登录接口的配置（还未具体实现）

---
+++ src/main/java/com/qxy/bytejump/entity/vo/Result.java
@@ -0,0 +1,30 @@
+package com.qxy.bytejump.entity.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result<T> {
+    /**
+     * 状态码
+     */
+    private Integer code;
+    /**
+     * 提示信息，如果有错误时，前端可以获取该字段进行提示
+     */
+    private String msg;
+    /**
+     * 查询到的结果数据，
+     */
+    private T data;
+
+    public static Result success(Integer code, String msg, Object data){
+        Result tResult = new Result(code,msg,data);
+        return tResult;
+    }
+}
