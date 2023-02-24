894a6ca4e2735544f982094d7de1265c8c2c4720
leeqi10 <447789814@qq.com>
2023-02-24 11:07:52 +0800

    基础接口与社交接口全部实现

--- data/simplybyte.sql
+++ data/simplybyte.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80029
  File Encoding         : 65001
 
- Date: 23/02/2023 18:20:35
+ Date: 24/02/2023 15:29:05
 */
 
 SET NAMES utf8mb4;
@@ -28,7 +28,7 @@ CREATE TABLE `comment`  (
   `create_data` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   `video_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 32 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 47 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of comment
@@ -57,6 +57,14 @@ INSERT INTO `comment` VALUES (28, '1234567', '牛', 'Thu Feb 23 17:35:12 CST 202
 INSERT INTO `comment` VALUES (29, '1234567', '哈哈哈哈', 'Thu Feb 23 17:35:15 CST 2023', '37');
 INSERT INTO `comment` VALUES (30, '1234567', '不错呀', 'Thu Feb 23 17:35:23 CST 2023', '38');
 INSERT INTO `comment` VALUES (31, '447789814@qq.com', '哈哈哈', 'Thu Feb 23 18:12:59 CST 2023', '38');
+INSERT INTO `comment` VALUES (32, '777', '666', 'Thu Feb 23 18:34:55 CST 2023', '31');
+INSERT INTO `comment` VALUES (33, '12345678', '666', 'Thu Feb 23 21:56:00 CST 2023', '36');
+INSERT INTO `comment` VALUES (34, '12345678', '66666', 'Thu Feb 23 21:56:17 CST 2023', '36');
+INSERT INTO `comment` VALUES (40, '456', '厉害呀', 'Thu Feb 23 22:02:58 CST 2023', '38');
+INSERT INTO `comment` VALUES (42, '456', '厉害咯', 'Thu Feb 23 22:07:00 CST 2023', '39');
+INSERT INTO `comment` VALUES (44, '1234577', '不错的', 'Thu Feb 23 22:10:00 CST 2023', '37');
+INSERT INTO `comment` VALUES (45, '1234577', '休息', 'Thu Feb 23 22:10:08 CST 2023', '38');
+INSERT INTO `comment` VALUES (46, '1234577', '66', 'Thu Feb 23 22:15:58 CST 2023', '40');
 
 -- ----------------------------
 -- Table structure for message
@@ -85,7 +93,7 @@ CREATE TABLE `user`  (
   `is_follow` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'false',
   `password` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 31 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 36 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
@@ -96,6 +104,11 @@ INSERT INTO `user` VALUES (27, 'tqq', 0, 0, 'false', '$2a$10$8XlQOfOBeha.7MkcIGZ
 INSERT INTO `user` VALUES (28, 'hello', 0, 0, 'false', '$2a$10$FrdT4A5v/G.NY2UTWK.XNOGBwRFtOkZZNnvBhEadWLGB2Hea9WuRu');
 INSERT INTO `user` VALUES (29, 'xjzbjbz', 0, 0, 'false', '$2a$10$Zck4v77fVCmrHt/wVB/mge3nFJUztE1Sxlx1NWTrG7X8eNj2dp41a');
 INSERT INTO `user` VALUES (30, '1234567', 0, 0, 'false', '$2a$10$IrCX5AxBdbZKpgvtADSOdOl4SLCtPW6uIA4NEtcRC2q75k7oUXDcK');
+INSERT INTO `user` VALUES (31, '777', 0, 0, 'false', '$2a$10$xGhN.wFK9gekDr2wlJ2jMOr/9QuCtIIiqZtyOYgqDuWAu62qxGrb6');
+INSERT INTO `user` VALUES (32, '12345678', 0, 0, 'false', '$2a$10$gbCOmp2T4t4J.Wve2eXTT.ya2xVSMxb3SjFNJfxSik4n.E.K2ag7K');
+INSERT INTO `user` VALUES (33, '456', 0, 0, 'false', '$2a$10$BYrWCEZaszunnzi6I8A6Qej932MCJJ9hQKvFuuRFDEdo2zHbBsiYC');
+INSERT INTO `user` VALUES (34, '1234577', 0, 0, 'false', '$2a$10$TRDrhhLrT66tiKYugHKjrOG3eHZJMAd/rxU/o/Ga37pwaonNAOufi');
+INSERT INTO `user` VALUES (35, '4477898@qq.com', 0, 0, 'false', '$2a$10$sJlnKWUFYTRI6HupmOikx.dxX02eqa4A48/9yzJJ0TiwG2AJnT82i');
 
 -- ----------------------------
 -- Table structure for userlikevideo
@@ -107,13 +120,13 @@ CREATE TABLE `userlikevideo`  (
   `user_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `is_favorite` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'false',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 36 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of userlikevideo
 -- ----------------------------
 INSERT INTO `userlikevideo` VALUES (1, '31', '25', 'true');
-INSERT INTO `userlikevideo` VALUES (2, '32', '25', 'false');
+INSERT INTO `userlikevideo` VALUES (2, '32', '25', 'true');
 INSERT INTO `userlikevideo` VALUES (3, '33', '25', 'true');
 INSERT INTO `userlikevideo` VALUES (4, '32', '28', 'true');
 INSERT INTO `userlikevideo` VALUES (5, '31', '28', 'true');
@@ -135,6 +148,18 @@ INSERT INTO `userlikevideo` VALUES (20, '38', '25', 'false');
 INSERT INTO `userlikevideo` VALUES (21, '34', '25', 'true');
 INSERT INTO `userlikevideo` VALUES (22, '35', '25', 'true');
 INSERT INTO `userlikevideo` VALUES (23, '36', '25', 'true');
+INSERT INTO `userlikevideo` VALUES (24, '36', '31', 'true');
+INSERT INTO `userlikevideo` VALUES (25, '38', '31', 'true');
+INSERT INTO `userlikevideo` VALUES (26, '39', '31', 'true');
+INSERT INTO `userlikevideo` VALUES (27, '31', '31', 'true');
+INSERT INTO `userlikevideo` VALUES (28, '36', '32', 'true');
+INSERT INTO `userlikevideo` VALUES (29, '31', '33', 'true');
+INSERT INTO `userlikevideo` VALUES (30, '32', '33', 'true');
+INSERT INTO `userlikevideo` VALUES (31, '34', '34', 'false');
+INSERT INTO `userlikevideo` VALUES (32, '36', '34', 'false');
+INSERT INTO `userlikevideo` VALUES (33, '35', '34', 'true');
+INSERT INTO `userlikevideo` VALUES (34, '37', '34', 'false');
+INSERT INTO `userlikevideo` VALUES (35, '40', '34', 'true');
 
 -- ----------------------------
 -- Table structure for video
@@ -150,18 +175,21 @@ CREATE TABLE `video`  (
   `title` varchar(124) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '视频标题',
   `user_name` varchar(24) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '视频作者的名字',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 39 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 42 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of video
 -- ----------------------------
-INSERT INTO `video` VALUES (31, 'http://192.168.1.105:8084/video/25/share_2e1ced9e7830b8293c177ebe16c9d256.mp4', 'http://192.168.1.105:8084/Cover/25/share_2e1ced9e7830b8293c177ebe16c9d256.png', 4, 9, 'false', '你好', '447789814@qq.com');
-INSERT INTO `video` VALUES (32, 'http://192.168.1.105:8084/video/26/share_9e6d04477343491aea4ecd22389ae56f.mp4', 'http://192.168.1.105:8084/Cover/26/share_9e6d04477343491aea4ecd22389ae56f.png', 2, 4, 'false', '测试3', 'tt');
-INSERT INTO `video` VALUES (33, 'http://192.168.1.105:8084/video/26/1234.mp4', 'http://192.168.1.105:8084/Cover/26/1234.png', 3, 3, 'false', '流量', 'tt');
-INSERT INTO `video` VALUES (34, 'http://192.168.1.105:8084/video/28/share_9d432f3e8cd64d2424eb7cb6852e0b8e.mp4', 'http://192.168.1.105:8084/Cover/28/share_9d432f3e8cd64d2424eb7cb6852e0b8e.png', 3, 1, 'false', '哈哈哈', 'hello');
-INSERT INTO `video` VALUES (35, 'http://192.168.1.105:8084/video/28/share_0a8c6e0ece0ad443fd77508c8f21100b.mp4', 'http://192.168.1.105:8084/Cover/28/share_0a8c6e0ece0ad443fd77508c8f21100b.png', 4, 1, 'false', '中国', 'hello');
-INSERT INTO `video` VALUES (36, 'http://192.168.1.105:8084/video/29/share_f273b0c8196a80401e4476034902fb5a.mp4', 'http://192.168.1.105:8084/Cover/29/share_f273b0c8196a80401e4476034902fb5a.png', 2, 0, 'false', '嘻嘻嘻哈哈哈', 'xjzbjbz');
-INSERT INTO `video` VALUES (37, 'http://192.168.1.105:8084/video/29/share_86774aef1dba4c8ccfff374eb4f4d1f1.mp4', 'http://192.168.1.105:8084/Cover/29/share_86774aef1dba4c8ccfff374eb4f4d1f1.png', 2, 0, 'false', '嗯哼', 'xjzbjbz');
-INSERT INTO `video` VALUES (38, 'http://192.168.1.105:8084/video/30/share_2e1ced9e7830b8293c177ebe16c9d256.mp4', 'http://192.168.1.105:8084/Cover/30/share_2e1ced9e7830b8293c177ebe16c9d256.png', 1, 2, 'false', '不错', '1234567');
+INSERT INTO `video` VALUES (31, 'http://ucd.com.cn:8084/video/25/share_2e1ced9e7830b8293c177ebe16c9d256.mp4', 'http://ucd.com.cn:8084/Cover/25/share_2e1ced9e7830b8293c177ebe16c9d256.png', 6, 10, 'false', '你好', '447789814@qq.com');
+INSERT INTO `video` VALUES (32, 'http://ucd.com.cn:8084/video/26/share_9e6d04477343491aea4ecd22389ae56f.mp4', 'http://ucd.com.cn:8084/Cover/26/share_9e6d04477343491aea4ecd22389ae56f.png', 4, 4, 'false', '测试3', 'tt');
+INSERT INTO `video` VALUES (33, 'http://ucd.com.cn:8084/video/26/1234.mp4', 'http://ucd.com.cn:8084/Cover/26/1234.png', 3, 4, 'false', '流量', 'tt');
+INSERT INTO `video` VALUES (34, 'http://ucd.com.cn:8084/video/28/share_9d432f3e8cd64d2424eb7cb6852e0b8e.mp4', 'http://ucd.com.cn:8084/Cover/28/share_9d432f3e8cd64d2424eb7cb6852e0b8e.png', 3, 2, 'false', '哈哈哈', 'hello');
+INSERT INTO `video` VALUES (35, 'http://ucd.com.cn:8084/video/28/share_0a8c6e0ece0ad443fd77508c8f21100b.mp4', 'http://ucd.com.cn:8084/Cover/28/share_0a8c6e0ece0ad443fd77508c8f21100b.png', 5, 1, 'false', '中国', 'hello');
+INSERT INTO `video` VALUES (36, 'http://ucd.com.cn:8084/video/29/share_f273b0c8196a80401e4476034902fb5a.mp4', 'http://ucd.com.cn:8084/Cover/29/share_f273b0c8196a80401e4476034902fb5a.png', 4, 2, 'false', '嘻嘻嘻哈哈哈', 'xjzbjbz');
+INSERT INTO `video` VALUES (37, 'http://ucd.com.cn:8084/video/29/share_86774aef1dba4c8ccfff374eb4f4d1f1.mp4', 'http://ucd.com.cn:8084/Cover/29/share_86774aef1dba4c8ccfff374eb4f4d1f1.png', 2, 3, 'false', '嗯哼', 'xjzbjbz');
+INSERT INTO `video` VALUES (38, 'http://ucd.com.cn:8084/video/30/share_2e1ced9e7830b8293c177ebe16c9d256.mp4', 'http://ucd.com.cn:8084/Cover/30/share_2e1ced9e7830b8293c177ebe16c9d256.png', 2, 4, 'false', '不错', '1234567');
+INSERT INTO `video` VALUES (39, 'http://ucd.com.cn:8084/video/31/share_9d432f3e8cd64d2424eb7cb6852e0b8e.mp4', 'http://ucd.com.cn:8084/Cover/31/share_9d432f3e8cd64d2424eb7cb6852e0b8e.png', 1, 1, 'false', '厉害', '777');
+INSERT INTO `video` VALUES (40, 'http://ucd.com.cn:8084/video/34/share_f273b0c8196a80401e4476034902fb5a.mp4', 'http://ucd.com.cn:8084/Cover/34/share_f273b0c8196a80401e4476034902fb5a.png', 1, 1, 'false', '666', '1234577');
+INSERT INTO `video` VALUES (41, 'http://localhost:8084/video/11/VID_20230121_191709.mp4', 'ss', 0, 0, 'false', 'sd', '11');
 
 SET FOREIGN_KEY_CHECKS = 1;
