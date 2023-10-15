f7c1fef64537d379cedca0777567d5a4d113428c
leeqi10 <447789814@qq.com>
2023-10-15 21:12:34 +0800

    it`s a test,which make sure true.

--- src/main/java/com/qxy/bytejump/filter/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/qxy/bytejump/filter/JwtAuthenticationTokenFilter.java
@@ -10,6 +10,9 @@ import com.qxy.bytejump.utils.JwtUtil;
 import com.qxy.bytejump.utils.RedisCache;
 
 import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -31,11 +34,12 @@ import java.util.Objects;
  * @createDate 2022/3/1
  */
 @Component
+@Slf4j
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 
     @Autowired
     private RedisCache redisCache;
-
+    private final Logger logger = LoggerFactory.getLogger(JwtAuthenticationTokenFilter.class);
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         //获取token
@@ -50,7 +54,7 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
             Claims claims = JwtUtil.parseJWT(token);
             userId = claims.getSubject();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("用户token非法;{}",e.getMessage(),e);
             throw new RuntimeException("token非法");
         }
 
